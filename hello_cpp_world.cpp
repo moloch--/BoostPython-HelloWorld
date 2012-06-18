1934a8201b8fb6ce1952cc503897f6748165e87b
Moloch <joedemesy@gmail.com>
2012-06-18 02:23:48 -0700

    Added list example

--- hello_cpp_world.cpp
+++ hello_cpp_world.cpp
@@ -4,8 +4,9 @@
  *  Created on: Jun 17, 2012
  *      Author: moloch
  */
-#include <iostream>
 #include <string>
+#include <vector>
+#include <iostream>
 #include <boost/python.hpp>
 #include <python2.7/Python.h>
 
@@ -15,11 +16,19 @@ void python_init() {
 }
 
 char const* greet() {
-	return "hello, world";
+	return "Hello World!";
 }
 
 void display_str(std::string word) {
 	std::cout << word << std::endl;
+}
+
+void display_list(unsigned int len, boost::python::list& ls) {
+	std::cout << "Length: " << len << std::endl;
+	for (unsigned int index = 0; index < len; ++index) {
+		std::string data = boost::python::extract<std::string>(ls[index]);
+		std::cout << index << ": " << data << std::endl;
+	}
 }
 
 BOOST_PYTHON_MODULE(hello_cpp_world)
@@ -27,6 +36,7 @@ BOOST_PYTHON_MODULE(hello_cpp_world)
 	using namespace boost::python;
 	def("greet", greet);
 	def("display_str", display_str);
+	def("display_list", display_list);
 
 	/* It is required to have a function with the same name as the module */
 	def("hello_cpp_world", python_init);
