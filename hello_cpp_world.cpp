98f5ab924af91482cd6e3e0ebdee985da6dac26c
Moloch <joedemesy@gmail.com>
2012-06-18 01:52:40 -0700

    Added example with arg

--- hello_cpp_world.cpp
+++ hello_cpp_world.cpp
@@ -4,27 +4,30 @@
  *  Created on: Jun 17, 2012
  *      Author: moloch
  */
-
-#include <python2.7/Python.h>
+#include <iostream>
+#include <string>
 #include <boost/python.hpp>
+#include <python2.7/Python.h>
 
 /* Required constructor, does nothing */
 void python_init() {
 	NULL;
 }
 
-/* Return a string */
 char const* greet() {
 	return "hello, world";
+}
+
+void display_str(std::string word) {
+	std::cout << word << std::endl;
 }
 
 BOOST_PYTHON_MODULE(hello_cpp_world)
 {
 	using namespace boost::python;
 	def("greet", greet);
+	def("display_str", display_str);
 
-	/*
-	 * It is required to have a function with the same name as the module (maininit)
-	 */
+	/* It is required to have a function with the same name as the module */
 	def("hello_cpp_world", python_init);
 }
